5423a14962ff36e60fa4fb9172b199c0cc300be2
ella-chetvertak <olegcetvertak037@gmail.com>
2022-07-06 11:35:05 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+const object = {
+    a: 5,
+    b: { g: 8, y: 9, t: { q: 48 }},
+    x: 47
+}
+let newObject = Object.assign({}, object)
+newObject.b = {...object.b}
+newObject.b.t = {...object.b.t}
+
+    // For example
+
+newObject.q = 20
+console.log(object) //nothing changed
+console.log(newObject) // q = 20 now
